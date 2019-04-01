588aa4c30eed94c4d36c5c0f683f1a21e2f42ce8
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2019-04-01 14:07:39 +0200

    Trade&dependencies

---
+++ pokeputz-sharing-server/src/main/java/fr/ensibs/pokeputz/trading/MainTrade.java
@@ -0,0 +1,10 @@
+package fr.ensibs.pokeputz.trading;
+
+public class MainTrade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pokeputz-sharing-server/src/main/java/fr/ensibs/pokeputz/trading/Trader.java
@@ -0,0 +1,15 @@
+package fr.ensibs.pokeputz.trading;
+
+public class Trader {
+	
+	Trader()
+	{
+		
+	}
+	
+	Trader(String host, String port)
+	{
+		
+	}
+
+}
